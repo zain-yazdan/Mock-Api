ab119df2e7038af17b89cecd2ad90ca6d0e17f85
Zain Yazdan <zain.yazdan@scytalelabs.com>
2023-06-23 23:53:11 +0500

    index.js file updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end(`Hello! Go to item 1: <a href="${path}">${path}</a>`);
 });
 
 app.get('/api/item/:slug', (req, res) => {
