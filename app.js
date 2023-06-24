4387c062ff615ea5fe714894043bd353d39a1c77
Zain Yazdan <zain.yazdan@scytalelabs.com>
2023-06-24 18:02:06 +0500

    versal.json file updated

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
+const listEndpoints = require('express-list-endpoints')
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,11 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   // next(createError(404));
   // console.log("req", req);
-  return res.status(404).send(`NOT FOUND: ${req.url}`);
+  return res.status(404).send(`NOT FOUND: ${req.url}, endpoints List: ${JSON.stringify(listEndpoints(app))}`);
 });
 
 // error handler
