e4aed73227c3a4bcedf978b0ee3c66523b897553
Zain Yazdan <zain.yazdan@scytalelabs.com>
2023-06-24 17:30:14 +0500

    versal.json file updated

--- api/index.js
+++ api/index.js
@@ -1,92 +1,18 @@
-#!/usr/bin/env node
+const app = require('express')();
+const { v4 } = require('uuid');
 
-/**
- * Module dependencies.
- */
-
-var app = require('../app');
-var debug = require('debug')('test-app:server');
-var http = require('http');
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  // res.setHeader('Content-Type', 'text/html');
+  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item app.js: <a href="${path}">${path}</a>`);
 });
-server.on('error', onError);
-server.on('listening', onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+app.listen(3000);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+module.exports = app;
